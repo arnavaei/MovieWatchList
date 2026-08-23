613fba4d967b008ad874081f1df80f7d3c61f0ae
Yousof Arnavaei <arnavaei@outlook.com>
2026-08-23 11:25:28 +0330

    Package Update

--- obj/Debug/net10.0/MovieWatchList.AssemblyInfo.cs
+++ obj/Debug/net10.0/MovieWatchList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieWatchList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be9bd43706d8de96a549f2d8845b9d7c9d402dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1dc62e419be6c6efc383bb77bdd2f5eead45f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieWatchList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieWatchList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
